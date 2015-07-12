34324258c5ee684913071702d2cae6d190bd7ffb
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2015-07-12 16:54:37 +0300

    simplify

--- adagram/infer.py
+++ adagram/infer.py
@@ -3,16 +3,10 @@ from __future__ import print_function, division
 import numpy as np
 
 
-def nearest_neighbors(vm, dictionary, word, sense, max_neighbours=10):
-    w_id = dictionary.word2id[word]
-    s_v = vm.InNorm[w_id, sense]
-    return _nearest_neighbors(
-        vm, dictionary, s_v, max_neighbours,
-        exclude=(w_id, sense))
-
-
-def _nearest_neighbors(vm, dictionary, s_v, max_neighbours,
-        exclude, min_count=1):
+def nearest_neighbors(vm, dictionary, word, sense,
+        max_neighbours=10, min_count=1):
+    word_id = dictionary.word2id[word]
+    s_v = vm.InNorm[word_id, sense]
     sim_matrix = np.dot(vm.InNorm, s_v)
     most_similar = []
     while len(most_similar) < max_neighbours:
@@ -20,6 +14,6 @@ def _nearest_neighbors(vm, dictionary, s_v, max_neighbours,
         w_id, s = idx // vm.prototypes, idx % vm.prototypes
         sim = sim_matrix[w_id, s]
         sim_matrix[w_id, s] = -np.inf
-        if (w_id, s) != exclude and vm.counts[w_id, s] > min_count:
+        if (w_id, s) != (word_id, sense) and vm.counts[w_id, s] >= min_count:
             most_similar.append((dictionary.id2word[w_id], s, sim))
     return most_similar
